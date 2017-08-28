8200621a6e92fe7246a680f71899264de3070d02
EmanBsisou <emanb83@yahoo.com>
2017-08-29 10:16:34 +1200

    Added MvcConfig and login page

---
+++ src/main/java/sos/MvcConfig.java
@@ -0,0 +1,18 @@
+package sos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");//home
+        registry.addViewController("/").setViewName("index");//home
+        registry.addViewController("/index").setViewName("index");//hello
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}

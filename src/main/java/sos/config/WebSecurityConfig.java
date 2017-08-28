8f790647be3aa34c351c8424798d714713b031ed
EmanBsisou <emanb83@yahoo.com>
2017-08-29 11:32:40 +1200

    WebSecurityConfig disable

--- src/main/java/sos/config/WebSecurityConfig.java
+++ src/main/java/sos/config/WebSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+  /*  @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
@@ -31,4 +31,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .inMemoryAuthentication()
                 .withUser("user").password("password").roles("ADMIN");
     }
+    */
 }

881f07d538d9814c0758521ff24875ce1283f403
EmanBsisou <emanb83@yahoo.com>
2017-08-29 11:43:38 +1200

    MvcConfig class

--- src/main/java/sos/MvcConfig.java
+++ src/main/java/sos/MvcConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
-/*
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/index").setViewName("index");//home
+       // registry.addViewController("/index").setViewName("index");//home
         registry.addViewController("/").setViewName("index");//home
         registry.addViewController("/index").setViewName("index");//hello
         registry.addViewController("/login").setViewName("login");
     }
-*/
+
 }

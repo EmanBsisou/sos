3c94658c402341c22f3c5e09c5b497eb68629051
EmanBsisou <emanb83@yahoo.com>
2017-08-29 20:55:27 +1200

    http get spring

--- src/main/resources/static/application.js
+++ src/main/resources/static/application.js
@@ -9,18 +9,29 @@ app.config(['$httpProvider','$logProvider','$routeProvider',
 		$routeProvider
 		.when('/', {
 		    controller: 'MainController',
-		    templateUrl: 'index.html'
+		    templateUrl: 'index.html',
+			controllerAs: 'controller'
 		})
 		.when('/download',{
 			templateUrl: 'download.html'
 		})
 				.when('/login',{
-			templateUrl: 'login.html'
+			templateUrl: 'login.html',
+			controller: 'navigation',
+			controllerAs: 'controller'
 		})
 		.otherwise({
 		    redirectTo: '/'
 		});
+		
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 	}
 ])
-
+  .controller('index', function($http) {
+    var self = this;
+    $http.get('/{userId}/').then(function(response) {
+      self.userId = response.data;
+    })
+  })
+  .controller('navigation', function() {});
 

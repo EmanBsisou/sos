1148c40aae21d9c09b6e2c57be6ec6591a730015
EmanBsisou <emanb83@yahoo.com>
2017-09-04 07:02:08 +1200

    added antMatchers

--- src/main/java/sos/config/WebSecurityConfig.java
+++ src/main/java/sos/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
@@ -29,15 +30,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http
-	    .csrf().disable().cors().disable().httpBasic().and().authorizeRequests()
-	    .anyRequest().authenticated();//leave till later as this disables GET too and then wont be able to open mobile app
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+	    .csrf().disable().cors().disable().httpBasic()
+	    .and().authorizeRequests()
+	    .antMatchers("/api/user/{userId}").permitAll().anyRequest().authenticated();
 	}
 	
+	/*
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
+	*/
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser("user").password("passcode").roles("USER");
     }
     
 }

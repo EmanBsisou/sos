24ebb6ec973f670675858b8505ce1614ac6ff465
EmanBsisou <emanb83@yahoo.com>
2017-09-03 19:57:04 +1200

    security config

--- src/main/java/sos/config/WebSecurityConfig.java
+++ src/main/java/sos/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http.csrf().disable().cors().disable().httpBasic().and().authorizeRequests().antMatchers("/", "/login").permitAll()
+	    http.csrf().disable().cors().disable().httpBasic().and().authorizeRequests()
 	    .anyRequest().authenticated();
 	}
 	

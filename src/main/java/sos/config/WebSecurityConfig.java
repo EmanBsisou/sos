85067dc81936ac6167c65181d7c2e2905900fa6c
EmanBsisou <emanb83@yahoo.com>
2017-09-03 20:53:31 +1200

    authentication

--- src/main/java/sos/config/WebSecurityConfig.java
+++ src/main/java/sos/config/WebSecurityConfig.java
@@ -29,8 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http
-	    .csrf().disable().cors().disable().httpBasic().and().authorizeRequests();/*
-	    .anyRequest().authenticated();*///leave till later as this disables GET too and then wont be able to open mobile app
+	    .csrf().disable().cors().disable().httpBasic().and().authorizeRequests()
+	    .anyRequest().authenticated();//leave till later as this disables GET too and then wont be able to open mobile app
 	}
 	
     @Autowired

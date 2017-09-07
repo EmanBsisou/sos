273b07469d38542ba81d6dd7fc62ebbdcbfb3ca4
EmanBsisou <emanb83@yahoo.com>
2017-09-07 12:54:09 +1200

    application.js updates

--- src/main/resources/static/application.js
+++ src/main/resources/static/application.js
@@ -30,10 +30,52 @@ app.config(['$httpProvider','$logProvider','$routeProvider',
 ])
  .controller('index', function($http) {
     var self = this;
-    $http.get('/{userId}/').then(function(response) {
+    $http.get('/api/{userId}/').then(function(response) {
      self.userId = response.data;
     })
   })
-  .controller('navigation', function() {});
+  .controller('navigation', function($rootScope, $http, $location) {
 
+  var self = this
+
+  var authenticate = function(credentials, callback) {
+
+    var headers = credentials ? {authorization : "Basic "
+        + btoa(credentials.username + ":" + credentials.password)
+    } : {};
+
+    $http.get('user', {headers : headers}).then(function(response) {
+      if (response.data.name) {
+        $rootScope.authenticated = true;
+      } else {
+        $rootScope.authenticated = false;
+      }
+      callback && callback();
+    }, function() {
+      $rootScope.authenticated = false;
+      callback && callback();
+    });
+
+  }
+
+  authenticate();
+  self.credentials = {};
+  self.login = function() {
+      authenticate(self.credentials, function() {
+        if ($rootScope.authenticated) {
+          $location.path("/");
+          self.error = false;
+        } else {
+          $location.path("/login");
+          self.error = true;
+        }
+      });
+  };
+});
+self.logout = function() {
+  $http.post('logout', {}).finally(function() {
+    $rootScope.authenticated = false;
+    $location.path("/");
+  });
+}
 	  

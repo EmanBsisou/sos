147e29e0849170ff8775062817af1ce75bc2fa7c
EmanBsisou <emanb83@yahoo.com>
2017-09-09 12:21:08 +1200

    updated js

--- src/main/resources/static/application.js
+++ src/main/resources/static/application.js
@@ -29,7 +29,7 @@ app.config(['$httpProvider','$logProvider','$routeProvider',
 	}
 ])
 
-
+/*
  .controller('index', function($http) {
     var self = this;
     $http.get('/api/{userId}/').then(function(response) {

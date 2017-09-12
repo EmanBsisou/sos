f4438c3c6b2182925d2c595b20b72acee36581c8
EmanBsisou <emanb83@yahoo.com>
2017-09-13 10:12:05 +1200

    removed security

--- src/main/java/sos/ApbApplication.java
+++ src/main/java/sos/ApbApplication.java
@@ -14,10 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+/*import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfToken;*/
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +61,7 @@ public class ApbApplication {//added extends...
 	        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	  }
 	}*/
-	
+	/*
 	public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		  @Override
 		  protected void configure(HttpSecurity http) throws Exception {
@@ -91,5 +91,5 @@ public class ApbApplication {//added extends...
 		    }
 		    filterChain.doFilter(request, response);
 		  }
-		}
+		}*/
 }

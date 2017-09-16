d9123d4213e914d1200bd64e743fbcb9a86df2c0
EmanBsisou <emanb83@yahoo.com>
2017-09-16 21:57:12 +1200

    angular updates

--- src/main/resources/static/application.js
+++ src/main/resources/static/application.js
@@ -11,9 +11,11 @@ app.config(['$httpProvider','$logProvider','$routeProvider',
 		  $httpProvider.defaults.headers.post = {};
 		  $httpProvider.defaults.headers.put = {};
 		  $httpProvider.defaults.headers.patch = {};
-		
+		*/
 		  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-		  */
+		  
+		  
+		 //$http.defaults.headers.common['Authorization'] = 'Bearer ' + access_token={accessToken};
 		
 		$routeProvider
 		.when('/', { //when I go to / I want to use MainController
@@ -35,7 +37,29 @@ app.config(['$httpProvider','$logProvider','$routeProvider',
 		});		
 	}
 ])
+// Creating the Angular Controller
+app.controller('AppCtrl', function($http, $scope) {
 
+	// method for getting user details
+	var getUser = function() {
+		$http.get('/user').success(function(user) {
+			$scope.user = user;
+			console.log('Logged User : ', user);
+		}).error(function(error) {
+			$scope.resource = error;
+		});
+	};
+	getUser();
+
+	// method for logout
+	$scope.logout = function() {
+		$http.post('/logout').success(function(res) {
+			$scope.user = null;
+		}).error(function(error) {
+			console.log("Logout error : ", error);
+		});
+	};
+});
 /*
 $http({
   method: 'GET',
